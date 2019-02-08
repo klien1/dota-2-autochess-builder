a6a8b251be37e005b8e048e563762f41dce2e057
Kevin Lien <klien8589@gmail.com>
2019-02-07 19:36:57 -0800

    removed redux form

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import selectedHeroes from './selectedHeroReducer';
 import countHeroes from './heroCountReducer';
 import heroData from './fetchHeroDataReducer';
-import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
 	selectedHeroes,
 	countHeroes,
-	heroData,
-	form: formReducer
+	heroData
 });
