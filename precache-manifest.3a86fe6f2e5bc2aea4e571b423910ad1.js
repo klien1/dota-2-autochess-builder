0c1231f81832e77c27c787ca904011558a9d322c
Kevin Lien <klien8589@gmail.com>
2019-02-08 10:30:08 -0800

    Updates

---
+++ precache-manifest.3a86fe6f2e5bc2aea4e571b423910ad1.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "55fc45e891ab9fd622d7d345c50042ae",
+    "url": "/dota-2-autochess-builder/static/media/Warrior.55fc45e8.png"
+  },
+  {
+    "revision": "ef45424ce9c70ca75d8f",
+    "url": "/dota-2-autochess-builder/static/css/main.387b2ad6.chunk.css"
+  },
+  {
+    "revision": "7edc398771fd2117f69d837376c92ac2",
+    "url": "/dota-2-autochess-builder/static/media/DonkeyCourier.7edc3987.jpg"
+  },
+  {
+    "revision": "6e939525bf4b59c26c4b",
+    "url": "/dota-2-autochess-builder/static/js/1.6e939525.chunk.js"
+  },
+  {
+    "revision": "7f1454e35fbe94215447",
+    "url": "/dota-2-autochess-builder/static/js/runtime~main.7f1454e3.js"
+  },
+  {
+    "revision": "aec6c3b04275de9737f7e6cffefa5aaf",
+    "url": "/dota-2-autochess-builder/static/media/Assassin.aec6c3b0.png"
+  },
+  {
+    "revision": "87edbcee258e011ed2ceda9ee355d10f",
+    "url": "/dota-2-autochess-builder/static/media/Beast.87edbcee.png"
+  },
+  {
+    "revision": "4075d8560829de26b89540ca76ba9b08",
+    "url": "/dota-2-autochess-builder/static/media/Demon Hunter.4075d856.png"
+  },
+  {
+    "revision": "6209288704bd215b6793543fb5618b13",
+    "url": "/dota-2-autochess-builder/static/media/Demon.62092887.png"
+  },
+  {
+    "revision": "b6d20ffc6ab5418ed7559d26b1fdab84",
+    "url": "/dota-2-autochess-builder/static/media/Dragon.b6d20ffc.png"
+  },
+  {
+    "revision": "6ea32f5b3220d54fa35e14dd606876d2",
+    "url": "/dota-2-autochess-builder/static/media/Druid.6ea32f5b.png"
+  },
+  {
+    "revision": "0dc7c5419aafe7e5dff8f79f94f0712f",
+    "url": "/dota-2-autochess-builder/static/media/Dwarf.0dc7c541.png"
+  },
+  {
+    "revision": "fc4961228362106c21e668ee77629b0f",
+    "url": "/dota-2-autochess-builder/static/media/Element.fc496122.png"
+  },
+  {
+    "revision": "ff8d1a6a55f52a3a703cdecf5131c645",
+    "url": "/dota-2-autochess-builder/static/media/Elf.ff8d1a6a.png"
+  },
+  {
+    "revision": "d14cbcedb5f1b084d309ca1d1b42d8b3",
+    "url": "/dota-2-autochess-builder/static/media/Goblin.d14cbced.png"
+  },
+  {
+    "revision": "81f800de28b597bcd6127e108d4591e6",
+    "url": "/dota-2-autochess-builder/static/media/Human.81f800de.png"
+  },
+  {
+    "revision": "0ebc7189a8898905413bd5ada8e47e5a",
+    "url": "/dota-2-autochess-builder/static/media/Hunter.0ebc7189.png"
+  },
+  {
+    "revision": "ee7a865d5bc2e76ab686be5916e25904",
+    "url": "/dota-2-autochess-builder/static/media/Knight.ee7a865d.png"
+  },
+  {
+    "revision": "6da596cc0e6fdb0b98153ceded5b3a32",
+    "url": "/dota-2-autochess-builder/static/media/Mage.6da596cc.png"
+  },
+  {
+    "revision": "40a4a8867735e811fd516d58322fb479",
+    "url": "/dota-2-autochess-builder/static/media/Mech.40a4a886.png"
+  },
+  {
+    "revision": "c9c35ba58a4aa8c47be62ac20a083be0",
+    "url": "/dota-2-autochess-builder/static/media/Naga.c9c35ba5.png"
+  },
+  {
+    "revision": "a21f3ab59fb78439feef432998873fb9",
+    "url": "/dota-2-autochess-builder/static/media/Ogre.a21f3ab5.png"
+  },
+  {
+    "revision": "a1b411a1ea34eaa571c6e7485aa4f83a",
+    "url": "/dota-2-autochess-builder/static/media/Orc.a1b411a1.png"
+  },
+  {
+    "revision": "f28d0eb44a33932012c875e64a324fc9",
+    "url": "/dota-2-autochess-builder/static/media/Shaman.f28d0eb4.png"
+  },
+  {
+    "revision": "363ec2b7afa7e436d88cb05865a7a273",
+    "url": "/dota-2-autochess-builder/static/media/Troll.363ec2b7.png"
+  },
+  {
+    "revision": "9f6f8ad20f8faada18eb13a7681616c4",
+    "url": "/dota-2-autochess-builder/static/media/Undead.9f6f8ad2.png"
+  },
+  {
+    "revision": "016113cadb2ccf0c50ea530c4fdd2c42",
+    "url": "/dota-2-autochess-builder/static/media/Warlock.016113ca.png"
+  },
+  {
+    "revision": "ef45424ce9c70ca75d8f",
+    "url": "/dota-2-autochess-builder/static/js/main.ef45424c.chunk.js"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/dota-2-autochess-builder/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "6e939525bf4b59c26c4b",
+    "url": "/dota-2-autochess-builder/static/css/1.eb82d2db.chunk.css"
+  },
+  {
+    "revision": "0bf285b99b0ce8724523bddcbcb86f49",
+    "url": "/dota-2-autochess-builder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dota-2-autochess-builder/precache-manifest.ea4a1e6182310006cd1fc3086a75a0b3.js"
+  "/dota-2-autochess-builder/precache-manifest.3a86fe6f2e5bc2aea4e571b423910ad1.js"
 );
 
 workbox.clientsClaim();
